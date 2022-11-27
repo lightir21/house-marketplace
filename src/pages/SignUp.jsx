a665527fa397b5b4f636eaf44060828b734bda11
meir <lightir21@gmail.com>
2022-11-27 12:50:54 +0200

    Add user to users collection in firebase
    SignUp.jsx:
    	-import setDoc, doc, serverTimestamp from firebase/firestore
    	-inside of the try/catch block in the onSubmit function
    		-create a formDataCopy const and use the spread operator inside curly braces // we dont want to change the original object so we create a new one and spread it inside
    		-use the delete key word on formDataCopy.password // so we wont upload the user password to the database
    		-add the timestamp field and set it to serverTimestamp function from firebase ->       formDataCopy.timestamp = serverTimestamp();
    		-await setDoc it take 2 argument
    			-first is the document so use -> doc(db,"users,user.uid") //the db object from out config file then the name of the collection (it will create it if non existent) and then we specify the name/id of the document which will be the same as in the user auth
    			-second the formDataCopy

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
@@ -39,10 +40,16 @@ const SignUp = () => {
         password
       );
 
-      const user = userCredential.userCredential;
+      const user = userCredential.user;
       updateProfile(auth.currentUser, {
         displayName: name,
       });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
 
       navigate("/");
     } catch (error) {
