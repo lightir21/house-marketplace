4adda255af0a608ecec6912d6437d4027211be05
meir <lightir21@gmail.com>
2022-11-27 12:28:00 +0200

    Signup with firebase auth
    Signup.jsx:
    	-import   getAuth, createUserWithEmailAndPassword, updateProfile from "firebase/auth";
    	-import db from firebase.confing
    	-set onSubmit on the form and set onSubmit function on it
    	-create a function called onSubmit make it async and give it e as a parameter
    		-e.preventDefault()
    		-create try/catch block
    		-create an auth const and invoke getAuth on it
    		-create userCredential const and await createUserWithEmailAndPassword pass in auth,email,password
    		-create a const user and set userCredential.user
    		-invoke updateProfile and pass in auth.currentUser and {displayName: name} // will set the display name of the user as the name that was passed in
    		-navigate to "/"
    		in the catch block just log the error

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -21,6 +27,29 @@ const SignUp = () => {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.userCredential;
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
@@ -29,7 +58,7 @@ const SignUp = () => {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="text"
               className="nameInput"
