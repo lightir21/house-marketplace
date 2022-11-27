1fd1e4f563f87df580b3d7dad36493430f3095e5
meir <lightir21@gmail.com>
2022-11-27 23:35:09 +0200

    SignIn functionality
    SignIn.jsx
    	-import getAuth, signInWithEmailAndPassword
    	-on the form set an onSubmit and pass in onSubmit function
    	-create inSubmit function make it async and pass in e as a parameter
    	-e.preventDefault()
    	-create a try/catch block
    	-inside the try->
    		-create a const auth and invoke getAuth on it
    		-create a const userCredential and await signInWithEmailAndPassword pass in auth, email and password
    		-create an if statement -> if userCredential.user then navigate to "/"
    	-catch block console.log the error
    
    App.js:
    	-change profile path element to Profile
    
    Profile.jsx:
    	-import useState and useEffect from react and import getAuth from firebase/auth
    	-create a state called user
    	-create a const auth and invoke getAuth on it
    	-create a useEffect with empty dependecie array setUser to auth.currentUser
    	-return ternary operator if user then show user.displayName if not "Not logged in"

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import { async } from "@firebase/util";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -20,6 +22,26 @@ const SignIn = () => {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
@@ -28,7 +50,7 @@ const SignIn = () => {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               className="emailInput"
