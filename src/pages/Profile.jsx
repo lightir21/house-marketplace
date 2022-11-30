907d5b33483dbb62898f72ff061c7c14f896bf45
meir <lightir21@gmail.com>
2022-11-30 13:31:03 +0200

    Update displayName in firebase
    Profile.jsx:
    	-import doc and updateDoc from firestore and import updateProfile from auth
    	-import db from firebase config file
    	-import toast
    	-create a new state called chageDetails and set it default to false
    	-create an onSubmit function and make it async
    		-create try/catch block
    			-try:
    				-if auth.currentUser.displayName do not equal name // from the state
    				// to update in firebase auth
    					-then await updateProfile -> pass in 2 parameters auth.currentUser and and object with what u would like to change in our case name
    				// to update in Firestore
    					-then create a userRef -> doc() pass in 3 parameters, db, cellection which is "users" and the uid of the user document -> doc(db, "users", auth.currentUser.uid);
    					-await updateDoc -> pass in userRef and an object with what u want to change in our case name
    			-catch:
    				-toast.error('could not update profile details')
    	-create onChnage pass in - e - as parameter
    		-setFormData((prevState) => ({...prevState,[e.target.id]: e.target.value})); // takes the previous state and spread it apart into the new object and then select the key and value from e.target
    	-return  a main tag
    		-make a p tag with an onClick and set it to a function
    			-check if changeDetails is true if so run the onSubmit function
    			-setChangeDetails((prevState) => !prevState); // will toggle from false to true
    			-make the a conditional for the text if changeDetails is true return "done" if false return "change"
    		-make a form with 2 inputs
    			1-  id="name"
    				className conditional if changeDetails is false return "profileName" if true "profileNameActive"
    				disabled if changeDetals is true
    				value name
    				onChange with the function of onChange
    			2-  id="email"
    				className conditional if changeDetails is false return "profileEmail" if true "profileEmailActive"
    				disabled if changeDetals is true
    				value email
    				onChange with the function of onChange

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
 import { useNavigate, Link } from "react-router-dom";
+import { async } from "@firebase/util";
+import { toast } from "react-toastify";
 
 const Profile = () => {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -18,6 +23,32 @@ const Profile = () => {
     navigate("/");
   };
 
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update display name in Firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // Update in Firestore
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error("Could not update profile details");
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -26,6 +57,42 @@ const Profile = () => {
           Logout
         </button>
       </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
