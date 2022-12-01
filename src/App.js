edacf16ee57d9b2091bad2860a52acc76d8766ea
meir <lightir21@gmail.com>
2022-12-01 13:23:52 +0200

    Forgot Password page
    ForgotPassword.jsx:
    	-import getAuth, sendPasswordResetEmail
    	-import useState
    	-import Link
    	-import toast
    	-import ReactComponent as ArrowRightIcon from assets folder
    	-create a state called email and set it default to empty string
    	-create onChange function set e as parameter and return setEmail(e.targer.value)
    	-create onSubmit function make it async and set e as a parameter
    		-e.preventDefault
    		-create try/catch block
    			try:
    				-create const auth invoke getAuth on that
    				-await sendPasswordResetEmail which accept 2 parameters 1 is auth 2 is email
    				-toast.success pass in Email was sent
    			catch(error):
    				-toast.error pass in Cold not send reset email
    
    	-in the return:
    		-create a header to show it's forgot password page
    		-in the main tag create a form with onSubmit function and with 1 email input value of email and onChange function
    		-under the input create a Link to /sign-in
    		-create a button to send the reset link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import ForgotPassword from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
