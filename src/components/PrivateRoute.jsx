2b386a1317220671a1cb807a23cfe27ebd6aa8ab
meir <lightir21@gmail.com>
2022-12-01 07:52:50 +0200

    useAuthStatus custome hook
    -in order to display the data after we're not logged in we have to make a hook that handles our "loading state"
    -create a new folder called hooks
    -inside that folder create a file called useAuthStatus.js
    
    useAuthStatus.js:
    	-import useEffect and useState
    	-import getAuth and onAuthStateChanged
    	-create a state called loggedIn and set it default to false
    	-create a state called checkingStatus and set it default to true
    	-create a useEffect
    		-creat a const auth and invoke getAuth
    		-onAuthStateChanged get 2 parameters 1- auth  2- a function that get the user object as a parameter
    			-create an if statement in this function and check if user then setLoggenIn to true
    			-setCheckingStatus to false and return loggedIn and checkingStatus

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
 
 const PrivateRoute = () => {
-  const loggedIn = true;
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
