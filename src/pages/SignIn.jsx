25c62119e8efd46eeedb5321ba4e766259ee5762
meir <lightir21@gmail.com>
2022-12-01 13:59:56 +0200

    SignIn/Up with Google
    -create a new component called OAuth.jsx
    
    OAuth.jsx:
    -import React from "react";
    -import { useLocation, useNavigate, useNavigation } from "react-router-dom";
    -import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
    -import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
    -import { db } from "../firebase.config";
    -import { toast } from "react-toastify";
    -import googleIcon from "../assets/svg/googleIcon.svg";
    
    -create const navigate and call useNavigate on it
    -create const location and call useLocation on it
    -create a fucntion called onGoogleClick make it async
    	-create a try/catch block
    		try:
    			-create const auth and call getAuth on it
    			-create const provider and call new GoogleAuthProvider on it
    			-create const result await signInWithPopup it accepts 2 parameters auth and the provider
    			-create const user and make it equal to result.user // provide the user we just created with google
    
    			// check for user
    			-create const docRef call doc on it and it take 3 parameters 1- dbm 2- "users" collection, 3- user.uid // refrencing the user with just logged from the users collection if it exist
    			-create const docSnap await getDoc and pass in docRef // gets the user data if there is any
    
    			// if user doesn't exit, create user
    			-create an if statement check if !docSnap.exists() then
    				-await setDoc(doc(db, "users", user.uid), {
              name: user.displayName,
              email: user.email,
              timeStamp: serverTimestamp(),
            })
            // it takes in the user data from google auth and place it into users collection inside of firestore

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -86,9 +87,7 @@ const SignIn = () => {
               </button>
             </div>
           </form>
-
-          {/* Goodgle OAuth */}
-
+          <OAuth />
           <Link to="/sign-up" className="registerLink">
             Sign Up Instead
           </Link>
